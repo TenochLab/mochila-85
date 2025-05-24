29ec3129dd7e989b1ba2c79fabb91e4719a52cd1
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2025-05-24 20:05:42 +0000

    Taliwind agregado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="p-5 bg-red-100">
+
+    </div>
   </main>
 </template>
